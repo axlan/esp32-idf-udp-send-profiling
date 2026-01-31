5d550fc614373ec389e6734939a77364e5395e6a
jdiamond <feros32@gmail.com>
2026-01-30 21:21:21 -0800

    bind to all interfaces.

--- scripts/connect_and_parse.sh
+++ scripts/connect_and_parse.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -e
 
-nc -kluvw 1 192.168.1.192 3333 | \
+nc -kluvw 1 0.0.0.0 3333 | \
 uv --project min-logger/python run min-logger-parser .pio/build/esp32_pio_min_logger.json \
   --log_format BINARY
